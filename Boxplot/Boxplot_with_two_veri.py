878addf90feffb6b16a4bdd8bd862c1f3fb8d08f
VANSH KUMAR <kumarvansh8956@gmail.com>
2026-08-01 23:35:50 +0530

    Add files via upload

---
+++ Boxplot/Boxplot_with_two_veri.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as ma
+import seaborn as s
+import pandas as p
+
+df = p.read_csv("car details v4.csv")
+print(df.head())
+
+print(df.columns)
+
+mak = df.groupby("Make")['Model'].count().sort_values(ascending=False).head(3)
+print(mak)
+
+Working_data = df.loc[df["Make"].isin(mak.index)]
+print(Working_data)
+ma.figure(figsize=(10,7))
+s.boxplot(data = Working_data , x= "Price", y ="Make",hue= "Fuel Type")
+ma.show()
